a6e25c4529281ccc8640f6918a3dc4de124df6bc
symphonyh <hxygsh@outlook.com>
2016-01-05 21:05:50 +0800

    pict small

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -2,22 +2,20 @@ $( document ).ready(function() {
 
 	// Set carousel options
 	$('.carousel').carousel({
-	  interval: 4000 // 4 seconds vs. default 5
+	  interval: 4000 
+	});
+
+	//Enable swiping...
+	$(".carousel-inner").swipe( {
+		//Generic swipe handler for all directions
+		swipeRight:function(event, direction, distance, duration, fingerCount) {
+			$(this).parent().carousel('prev');
+		},
+		swipeLeft: function() {
+			$(this).parent().carousel('next');
+		},
+		//Default is 75px, set to 0 so any distance triggers swipe
+		threshold:0
 	});
 
 });
-
-$( document ).ready(function() {
-
-  $('.navbar [href^=#]').click(function (e) {
-    e.preventDefault();
-    var div = $(this).attr('href');
-    $("html, body").animate({
-      scrollTop: $(div).position().top
-    }, "slow");
-  });
-
-});
-
-
-
