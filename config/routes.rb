ffe335399addfe8c28e9a76b3395f1e7ae9146af
symphonyh <hxygsh@outlook.com>
2016-01-23 11:20:46 +0800

    happy

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Bootstrap::Application.routes.draw do
   get "pages/home"
+   get "pages/about"
   root 'pages#home'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
